2488d958d2ebd2c831e95f234509a1593763edaf
webdevelopersrinu <159899608+webdevelopersrinu@users.noreply.github.com>
2024-08-09 17:54:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,66 @@
+const sideMenu = document.getElementById("sideMenu");
+const navBar = document.querySelector("nav");
+const navLinks = document.querySelector("nav ul");
+function openMenu() {
+  sideMenu.style.transform = "translateX(-16rem)";
+}
+function closeMenu() {
+  sideMenu.style.transform = "translateX(16rem)";
+}
+window.addEventListener("scroll", () => {
+  if (scrollY > 50) {
+    navBar.classList.add(
+      "bg-white",
+      "bg-opacity-50",
+      "backdrop-blur-lg",
+      "shadow-sm",
+      "dark:bg-darkTheme",
+      "dark:shadow-white/20"
+    );
+    navLinks.classList.remove(
+      "bg-white",
+      "bg-opacity-50",
+      "shadow-sm",
+      "dark:border",
+      "dark:border-white/50",
+      "dark:bg-transparent"
+    );
+  } else {
+    navBar.classList.remove(
+      "bg-white",
+      "bg-opacity-50",
+      "backdrop-blur-lg",
+      "shadow-sm",
+      "dark:bg-darkTheme",
+      "dark:shadow-white/20"
+    );
+    navLinks.classList.add(
+      "bg-white",
+      "bg-opacity-50",
+      "shadow-sm",
+      "dark:border",
+      "dark:border-white/50",
+      "dark:bg-transparent"
+    );
+  }
+});
+
+if (
+  localStorage.theme === "dark" ||
+  (!("theme" in localStorage) &&
+    window.matchMedia("(perfers-color-scheme:dark)").matches)
+) {
+  document.documentElement.classList.add("dark");
+} else {
+  document.documentElement.classList.remove("dark");
+}
+
+// toggle theme
+function toggleTheme() {
+  document.documentElement.classList.toggle("dark");
+  if (document.documentElement.classList.contains("dark")) {
+    localStorage.theme = "dark";
+  } else {
+    localStorage.theme = "light";
+  }
+}
